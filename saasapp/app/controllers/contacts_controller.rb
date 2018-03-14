4b54a31b7a05e12d804a6a2f41d622fea1045c0d
Clayton <ezzellclayton@gmail.com>
2018-03-14 22:24:06 +0000

    Trigger the mailer from the controller

--- saasapp/app/controllers/contacts_controller.rb
+++ saasapp/app/controllers/contacts_controller.rb
@@ -6,7 +6,12 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            comments = params[:contact][:comments]
+            ContactMailer.contact_email(name, email, comments).deliver
             flash[:success] = "Message Sent"
+
             redirect_to contact_us_path
         else
             flash[:danger] = @contact.errors.full_messages.join(", ")
