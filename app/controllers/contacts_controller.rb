5ccfff6f23afe8d16a76e3dc567deebdb77eca1a
Clayton <ezzellclayton@gmail.com>
2018-03-16 18:31:39 +0000

    Added code comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,34 @@
 class ContactsController < ApplicationController
+    
+    #Get request /contact-us
+    #Show new contact form
     def new
         @contact = Contact.new
     end
     
+    #Post request /contacts
+    #Flash and reroute to /contact us
     def create
+        #Mass assignment of form fields
         @contact = Contact.new(contact_params)
         if @contact.save
+            #Succeeded. Save, Deliver, Flash, Redirect
             name = params[:contact][:name]
             email = params[:contact][:email]
             comments = params[:contact][:comments]
+            #Send email with Mailgun
             ContactMailer.contact_email(name, email, comments).deliver
             flash[:success] = "Message Sent"
-
             redirect_to contact_us_path
         else
+            #Failed. Flash, Redirect
             flash[:danger] = @contact.errors.full_messages.join(", ")
             redirect_to contact_us_path
         end
     end
 
     private
+        # To collect Data From Form Fields, whitelist the form fields
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
         end
